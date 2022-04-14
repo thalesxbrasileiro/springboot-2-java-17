873a9de77c7f273194c65192c1e99baa6642dba5
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2022-04-14 19:08:38 -0300

    Adicionado o user entity e o user resource

---
+++ src/main/java/com/txb/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.txb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.txb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1, "Maria", "maria@gmail.com", "83989112478", "12345");
+		return ResponseEntity.ok().body(user);
+	}
+}
