0362fdb77f0e559f0db75f69e22c75945fcc1e2a
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2022-04-16 12:48:00 -0300

    Product

---
+++ src/main/java/com/txb/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.txb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.txb.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
