c9002ad440a8326e8c49bf5d9f5a20c6ff83257f
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2022-04-23 15:03:02 -0300

    Exception handling - delete

---
+++ src/main/java/com/txb/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,9 @@
+package com.txb.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+}
